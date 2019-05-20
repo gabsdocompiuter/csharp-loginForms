2dc2d9b07fad6ff5b822c6e4c594fbeb70a93f3b
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-20 10:12:09 -0300

    Adiciona o FrmLollipop e muda versão do .NET para 4.6

---
+++ loginForms/loginForms/FrmLollipop.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace loginForms
+{
+    partial class FrmLollipop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lollipopTextBox1 = new LollipopTextBox();
+            this.SuspendLayout();
+            // 
+            // lollipopTextBox1
+            // 
+            this.lollipopTextBox1.FocusedColor = "#508ef5";
+            this.lollipopTextBox1.FontColor = "#999999";
+            this.lollipopTextBox1.IsEnabled = true;
+            this.lollipopTextBox1.Location = new System.Drawing.Point(36, 54);
+            this.lollipopTextBox1.MaxLength = 32767;
+            this.lollipopTextBox1.Multiline = false;
+            this.lollipopTextBox1.Name = "lollipopTextBox1";
+            this.lollipopTextBox1.ReadOnly = false;
+            this.lollipopTextBox1.Size = new System.Drawing.Size(300, 24);
+            this.lollipopTextBox1.TabIndex = 0;
+            this.lollipopTextBox1.Text = "lollipopTextBox1";
+            this.lollipopTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lollipopTextBox1.UseSystemPasswordChar = false;
+            // 
+            // FrmLollipop
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(384, 218);
+            this.Controls.Add(this.lollipopTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmLollipop";
+            this.Text = "FrmLollipop";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LollipopTextBox lollipopTextBox1;
+    }
+}
